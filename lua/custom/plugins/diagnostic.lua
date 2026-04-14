83ca19aed59e0b097fd0005c5f8bda5e18021f8c
Matias Olivera <matias.olivera@naranjax.com>
2026-04-14 15:55:26 -0300

    fix: multiline error

--- lua/custom/plugins/diagnostic.lua
+++ lua/custom/plugins/diagnostic.lua
@@ -2,7 +2,13 @@ return {
   'rachartier/tiny-inline-diagnostic.nvim',
   event = 'VeryLazy',
   config = function()
-    require('tiny-inline-diagnostic').setup()
+    require('tiny-inline-diagnostic').setup {
+      options = {
+        multilines = {
+          enabled = true,
+        },
+      },
+    }
     -- Note: You MUST disable the default virtual text for this to look good
     vim.diagnostic.config { virtual_text = false }
   end,
